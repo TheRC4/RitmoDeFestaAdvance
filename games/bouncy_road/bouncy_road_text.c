93b89910e5c00def84babf6552ab235e2856e9d2
Nyoka <davijoaquimsilvacosta2@gmail.com>
2025-12-16 19:37:16 -0300

    Add files via upload

--- games/bouncy_road/bouncy_road_text.c
+++ games/bouncy_road/bouncy_road_text.c
@@ -7,25 +7,25 @@
 
 const char D_0805b778[] = "Fofocas do Vento";
 
-const char D_0805b784[] = "Voce precisa focar no basico.";
+const char D_0805b784[] = "Vocˆ® precisa focar no b‡csico.";
 
 const char D_0805b7a0[] = "";
 
-const char D_0805b7a4[] = "As bolas consecutivas deixaram voce em panico...";
+const char D_0805b7a4[] = "As bolas consecutivas deixaram vocˆ® em p‡unico...";
 
-const char D_0805b7cc[] = "Voce lidou com todas essas esferas com facilidade!";
+const char D_0805b7cc[] = "Vocˆ® lidou com todas essas esferas com facilidade!";
 
 const char D_0805b7f8[] = "Seu timing foi perfeito!";
 
 const char D_0805b810[] = "Fofocas do Vento";
 
-const char D_0805b820[] = "Voce precisa focar no basico.";
+const char D_0805b820[] = "Vocˆ® precisa focar no b‡csico.";
 
 const char D_0805b83c[] = "";
 
-const char D_0805b840[] = "As bolas consecutivas deixaram voce em panico...";
+const char D_0805b840[] = "As bolas consecutivas deixaram vocˆ® em p‡unico...";
 
-const char D_0805b868[] = "Voce lidou com todas essas esferas com facilidade!";
+const char D_0805b868[] = "Vocˆ® lidou com todas essas esferas com facilidade!";
 
 const char D_0805b894[] = "Mantenha a calma, seu timing estava ruim. ";
 
