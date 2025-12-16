8c67578c4197ff4b63c3407477d2e8bd9c25e014
Nyoka <davijoaquimsilvacosta2@gmail.com>
2025-12-16 19:40:30 -0300

    Add files via upload

--- games/bunny_hop/bunny_hop_text.c
+++ games/bunny_hop/bunny_hop_text.c
@@ -5,17 +5,17 @@
 /* Game Text - Bunny Hop */
 
 
-const char D_0805cbbc[] = "You were a little unsteady.";
+const char D_0805cbbc[] = "Voc旭 estava um pouco inst㌘vel.";
 
-const char D_0805cbd4[] = "Nice, steady rhythm!";
+const char D_0805cbd4[] = "Ritmo agrad㌘vel e constante!";
 
-const char D_0805cbec[] = "You couldn't find your flow...";
+const char D_0805cbec[] = "Voc旭 nao conseguiu encontrar seu ritmo...";
 
-const char D_0805cc10[] = "Your flow is on point!";
+const char D_0805cc10[] = "Seu ritmo est㌘ perfeito!";
 
-const char D_0805cc28[] = "You missed the breaks.";
+const char D_0805cc28[] = "Voc旭 perdeu as pausas.";
 
-const char D_0805cc48[] = "You nailed the breaks!";
+const char D_0805cc48[] = "Voc旭 arrasou nas pausas!";
 
 const char D_0805cc68[] = "Ｎｅｘｔ．．．";
 
@@ -35,7 +35,7 @@ const char D_0805cca0[] = "４";
 
 const char D_0805cca4[] = "ｒｅｓｔ　ｆｏｒ　ａｎ　ｅｉｇｈｔ－ｃｏｕｎｔ．";
 
-const char D_0805ccb4[] = "Sea Friends' Report";
+const char D_0805ccb4[] = "Relat㌍rio dos Amigos do Mar";
 
 const char D_0805ccb8[] = "　　　　　　　　　　　　　　　　　　　　Ｐｒｅｓｓ　ゝ　ｏｎ　ｂｅａｔ　ｔｏ　ｈｏｐ　ａｃｒｏｓｓ．";
 
