b0a65a18168a8d9b77f3415f2d9e450d9bce0761
Nyoka <davijoaquimsilvacosta2@gmail.com>
2025-12-16 21:01:26 -0300

    Add files via upload

--- games/drum_live/drum_live_text.c
+++ games/drum_live/drum_live_text.c
@@ -5,14 +5,14 @@
 /* Game Text - LIVE */
 
 
-const char D_0805d398[] = "That was rather messy. ";
+const char D_0805d398[] = "Isso foi uma bagunˆ¹a. ";
 
-const char D_0805d3b0[] = "So close to something cool! ";
+const char D_0805d3b0[] = "Tˆºo perto de algo legal! ";
 
-const char D_0805d3c4[] = "Something doesn't seem right with this. ";
+const char D_0805d3c4[] = "Algo nˆºo parece certo nisso. ";
 
-const char D_0805d3e4[] = "You nailed it!";
+const char D_0805d3e4[] = "Vocˆ® arrasou!";
 
-const char D_0805d404[] = "You were lucky you didn't trip yourself.";
+const char D_0805d404[] = "Vocˆ® teve sorte de nˆºo ter tropeˆ¹ado.";
 
-const char D_0805d420[] = "Seems like you have a lot of energy!";
+const char D_0805d420[] = "Parece que vocˆ® tem muita energia!";
