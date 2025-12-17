d3f23f2bb7add895f1a31a3c65784352b84e140c
Nyoka <davijoaquimsilvacosta2@gmail.com>
2025-12-16 21:03:46 -0300

    Add files via upload

--- games/karate_man/karate_man_2_text.c
+++ games/karate_man/karate_man_2_text.c
@@ -5,20 +5,20 @@
 /* Game Text - Karate Man 2 */
 
 
-const char D_0805b3a8[] = "You need to go over the basics again.";
+const char D_0805b3a8[] = "Vocˆ® precisa revisar os conceitos b‡csicos novamente.";
 
 const char D_0805b3c4[] = "";
 
-const char D_0805b3c8[] = "You were outpaced at the fast parts.";
+const char D_0805b3c8[] = "Vocˆ® foi ultrapassado nas partes r‡cpidas.";
 
-const char D_0805b3e8[] = "You were ahead of the fast parts!";
+const char D_0805b3e8[] = "Vocˆ® estava ‡g frente nas partes r‡cpidas!";
 
-const char D_0805b404[] = "The slow parts were simply chaotic.";
+const char D_0805b404[] = "As partes lentas eram simplesmente ca‡iticas.";
 
-const char D_0805b41c[] = "You kept your cool on the slow parts!";
+const char D_0805b41c[] = "Vocˆ® manteve a calma nas partes mais lentas!";
 
-const char D_0805b43c[] = "I'd hoped you wouldn't mess up the ending...";
+const char D_0805b43c[] = "Eu esperava que vocˆ® nˆºo estragasse o final...";
 
-const char D_0805b460[] = "Even the slowest parts were no problem!";
+const char D_0805b460[] = "Mesmo as partes mais lentas nˆºo foram problema!";
 
-const char D_0805b480[] = "The Master speaks...";
+const char D_0805b480[] = "O Mestre fala...";
--- games/karate_man/karate_man_text.c
+++ games/karate_man/karate_man_text.c
@@ -5,55 +5,55 @@
 /* Game Text - Karate Man */
 
 
-const char D_0805ab50[] = "You needed to focus on the basics.";
+const char D_0805ab50[] = "Vocˆ® precisava focar no b‡csico.";
 
 const char D_0805ab68[] = "";
 
-const char D_0805ab6c[] = "Your sequential punches were just sad.";
+const char D_0805ab6c[] = "Seus socos sequenciais estavam simplesmente tristes.";
 
-const char D_0805ab90[] = "Stellar sequential punches!";
+const char D_0805ab90[] = "Socos sequenciais incr‡eveis!";
 
-const char D_0805abb0[] = "You nailed the ending!";
+const char D_0805abb0[] = "Vocˆ® acertou o final!";
 
 const char D_0805abc4[] =
     "\n"
-    "Remember to feel the flow!";
+    "Lembre-se de sentir o ritmo!";
 
-const char D_0805abdc[] = "The Master speaks...";
+const char D_0805abdc[] = "O Mestre fala...";
 
 const char D_0805abec[] =
-    "Welcome to karate \n"
-    "training!";
+    "Bem-vindo ao\n"
+    "treinamento de caratˆ®!";
 
 const char D_0805ac0c[] = 
-    "Press ‡M to punch the\n"
-    "objects that are flying\n"
-    "your way.\n"
-    "The timing's a little tricky!";
+    "Pressione ‡M para socar os\n"
+    "objetos que estˆºo voando\n"
+    "na sua direˆ¹ˆºo.\n"
+    "O timing ‡Q meio complicado!";
 
 const char D_0805ac60[] =
-    "Here, why don't you give\n"
-    "it a shot?";
+    "Aqui, por que vocˆ® nˆºo\n"
+    "tenta um pouco?";
 
 const char D_0805aca0[] =
-    "See?\n"
-    "The timing is pretty\n"
-    "strict, isn't it?";
+    "Viu?\n"
+    "O timing ‡Q bem\n"
+    "rigoroso, n‡Q?";
 
 const char D_0805acd8[] =
-    "Now try punching to\n"
-    "the beat of some music!";
+    "Agora tente socar no\n"
+    "ritmo da mˆ¦sica!";
 
 const char D_0805ad04[] =
-    "How was that?\n"
-    "Much easier with the\n"
-    "music, right?";
+    "Como foi?\n"
+    "Bem mais f‡ccil com a\n"
+    "mˆ¦sica, n‡Q?";
 
 const char D_0805ad38[] =
-    "Well, there you have it!\n"
-    "Just flow with the music\n"
-    "and punch away!";
+    "Bom, ‡Q isso a‡e!\n"
+    "Apenas entre no ritmo\n"
+    "e continue socando!";
 
 const char D_0805ad80[] =
     "\n"
-    "Ready, set, punch!";
+    "Preparar, apontar, socar!";
