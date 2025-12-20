44d59db65c85fb4114dd1e92504304dc13651cd4
Nyoka <davijoaquimsilvacosta2@gmail.com>
2025-12-20 11:07:13 -0300

    Add files via upload

--- games/mr_upbeat/mr_upbeat_text.c
+++ games/mr_upbeat/mr_upbeat_text.c
@@ -5,34 +5,34 @@
 /* Game Text - Mr. Upbeat */
 
 
-const char D_08068170[] = "Hop over the needle!";
+const char D_08068170[] = "Pule por cima da agulha!";
 
-const char D_0806817c[] = "That's the way.";
+const char D_0806817c[] = "‡P assim mesmo.";
 
-const char D_08068188[] = "All right!";
+const char D_08068188[] = "Muito bem!";
 
-const char D_08068194[] = "Nice!";
+const char D_08068194[] = "Boa!";
 
-const char D_080681a0[] = "You're a needle-hopping ninja!";
+const char D_080681a0[] = "Vocˆ® ‡Q um ninja da agulha!";
 
-const char D_080681ac[] = "I'm so impressed by your skill, my feet are numb!";
+const char D_080681ac[] = "Estou tˆºo impressionado com a sua habilidade que meus p‡Qs estˆºo dormentes!";
 
-const char D_080681bc[] = "Yay, you!";
+const char D_080681bc[] = "Sim, vocˆ®!";
 
-const char D_080681c8[] = "How is it even possible that you're this good?";
+const char D_080681c8[] = "Como ‡Q possivel que vocˆ® seja tˆºo bom assim?";
 
-const char D_080681d8[] = "You're just doing this with mirrors, right?";
+const char D_080681d8[] = "Vocˆ® est‡c fazendo isso com espelhos, certo?";
 
-const char D_080681e8[] = "Were you bitten by something radioactive?";
+const char D_080681e8[] = "Vocˆ® foi mordido por algo radioativo?";
 
-const char D_080681f0[] = "HOW ARE YOU DOING THIS?!";
+const char D_080681f0[] = "COMO VOCˆ­ EST‡b FAZENDO ISSO?!";
 
-const char D_080681fc[] = "Nooo! It HURTS!";
+const char D_080681fc[] = "NAO! Isso d‡ii!";
 
-const char D_0806820c[] = "Can I stop now?";
+const char D_0806820c[] = "Posso parar agora?";
 
-const char D_0806821c[] = "...Help...me...";
+const char D_0806821c[] = "...Socorro...";
 
-const char D_0806822c[] = "Let me...rest...";
+const char D_0806822c[] = "Me deixe...descansar...";
 
-const char D_08068238[] = "So...tired... I think I can see through time...";
+const char D_08068238[] = "Tˆºo...cansado... Acho que consigo ver atrav‡Qs do tempo...";
