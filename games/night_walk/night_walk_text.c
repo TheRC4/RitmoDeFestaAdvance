d322463b9ff54ff6670c7d45587a4c11cae1e8a7
Nyoka <davijoaquimsilvacosta2@gmail.com>
2025-12-20 11:15:10 -0300

    Add files via upload

--- games/night_walk/night_walk_text.c
+++ games/night_walk/night_walk_text.c
@@ -5,46 +5,46 @@
 /* Game Text - Night Walk */
 
 
-const char D_0805b158[] = "You fell in a hole.";
+const char D_0805b158[] = "Vocˆ® caiu num buraco.";
 
-const char D_0805b174[] = "You made it to the end!";
+const char D_0805b174[] = "Vocˆ® chegou ao fim!";
 
-const char D_0805b18c[] = "Your rhythm was off.";
+const char D_0805b18c[] = "Seu ritmo estava ruim.";
 
-const char D_0805b1a4[] = "You kept the beat well.";
+const char D_0805b1a4[] = "Vocˆ® manteve bem o ritmo.";
 
-const char D_0805b1bc[] = "You missed quite a few jumps.";
+const char D_0805b1bc[] = "Vocˆ® errou v‡crios saltos.";
 
-const char D_0805b1dc[] = "Your jumps were right on the money!";
+const char D_0805b1dc[] = "Seus saltos foram perfeitos!";
 
-const char D_0805b1f4[] = "The Stars Say";
+const char D_0805b1f4[] = "As Estrelas Dizem";
 
-const char D_0805b1fc[] = "\x05\x31" "\x01\x35" "Jump in time with the music!";
+const char D_0805b1fc[] = "\x05\x31" "\x01\x35" "Entre no ritmo da mˆ¦sica!";
 
-const char D_0805b220[] = "\x05\x31" "\x01\x35" "Let's reach the stars before the music ends!";
+const char D_0805b220[] = "\x05\x31" "\x01\x35" "Vamos alcanˆ¹ar as estrelas antes que a mˆ¦sica termine!";
 
-const char D_0805b250[] = "\x05\x31" "\x01\x35" "Almost there!";
+const char D_0805b250[] = "\x05\x31" "\x01\x35" "Est‡c quase acabando!";
 
-const char D_0805b26c[] = "You fell in a hole.";
+const char D_0805b26c[] = "Vocˆ® caiu num buraco.";
 
-const char D_0805b288[] = "You made it to the end!";
+const char D_0805b288[] = "Vocˆ® chegou ao fim!";
 
-const char D_0805b2a0[] = "Your rhythm was off.";
+const char D_0805b2a0[] = "Seu ritmo estava ruim.";
 
-const char D_0805b2b8[] = "You kept the beat well.";
+const char D_0805b2b8[] = "Vocˆ® manteve bem o ritmo.";
 
-const char D_0805b2d0[] = "You missed quite a few jumps.";
+const char D_0805b2d0[] = "Vocˆ® errou v‡crios saltos.";
 
-const char D_0805b2f0[] = "Your jumps were right on the money!";
+const char D_0805b2f0[] = "Seus saltos foram perfeitos!";
 
-const char D_0805b308[] = "The Stars Say";
+const char D_0805b308[] = "As Estrelas Dizem";
 
-const char D_0805b310[] = "\x05\x31" "\x01\x35" "Jump in time with the music!";
+const char D_0805b310[] = "\x05\x31" "\x01\x35" "Entre no ritmo da mˆ¦sica!";
 
-const char D_0805b334[] = "\x05\x31" "\x01\x35" "Keep an eye out for electric fish!";
+const char D_0805b334[] = "\x05\x31" "\x01\x35" "Fique atento aos peixes el‡Qtricos!";
 
-const char D_0805b335[] = "\x05\x31" "\x01\x35" "Don't jump when you're under them!";
+const char D_0805b335[] = "\x05\x31" "\x01\x35" "Nˆºo pule quando estiver embaixo deles!";
 
-const char D_0805b35c[] = "\x05\x31" "\x01\x35" "Let's reach the stars before the music ends!";
+const char D_0805b35c[] = "\x05\x31" "\x01\x35" "Vamos alcanˆ¹ar as estrelas antes que a mˆ¦sica termine!";
 
-const char D_0805b38c[] = "\x05\x31" "\x01\x35" "Almost there!";
+const char D_0805b38c[] = "\x05\x31" "\x01\x35" "Quase l‡c!";
