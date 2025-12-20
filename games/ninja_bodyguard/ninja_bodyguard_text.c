78c8657006a4e2272d22f2382f7bde4c58c97144
Nyoka <davijoaquimsilvacosta2@gmail.com>
2025-12-20 11:28:25 -0300

    Add files via upload

--- games/ninja_bodyguard/ninja_bodyguard_text.c
+++ games/ninja_bodyguard/ninja_bodyguard_text.c
@@ -5,32 +5,32 @@
 /* Game Text - Ninja Bodyguard */
 
 
-const char D_0805b8e0[] = "I hired you to protect me.";
+const char D_0805b8e0[] = "Contratei vocˆ® para me proteger.";
 
 const char D_0805b8f8[] = "";
 
-const char D_0805b8fc[] = "Your swordplay skills were lacking.";
+const char D_0805b8fc[] = "Suas habilidades com a espada eram insuficientes.";
 
-const char D_0805b924[] = "You're quite nimble with a sword!";
+const char D_0805b924[] = "Vocˆ® ‡Q bastante ‡cgil com a espada!";
 
-const char D_0805b944[] = "Their four-arrow barrage overwhelmed you.";
+const char D_0805b944[] = "A sua barragem de quatro flechas o dominou.";
 
-const char D_0805b968[] = "You blocked their four-arrow barrage!";
+const char D_0805b968[] = "Vocˆ® bloqueou a saraivada de quatro flechas deles!";
 
-const char D_0805b988[] = "My Lord Solemnly Intones";
+const char D_0805b988[] = "Meu Senhor Entoa Solenemente";
 
-const char D_0805b99c[] = "I thought you were trying to protect me...";
+const char D_0805b99c[] = "Pensei que estivesse tentando me proteger...";
 
 const char D_0805b9b4[] = "";
 
-const char D_0805b9b8[] = "Your stick swipes weren't quick enough.";
+const char D_0805b9b8[] = "Seus golpes com a vara nao foram r‡cpidos o suficiente.";
 
-const char D_0805b9dc[] = "You're pretty quick with a stick!";
+const char D_0805b9dc[] = "Vocˆ® ‡Q muito r‡cpido com a vara!";
 
-const char D_0805b9fc[] = "Their five-rock barrage overwhelmed you.";
+const char D_0805b9fc[] = "A enxurrada de cinco pedras deles dominou vocˆ®.";
 
-const char D_0805ba1c[] = "You blocked their five-rock barrage!";
+const char D_0805ba1c[] = "Vocˆ® bloqueou a enxurrada de cinco pedras deles!";
 
-const char D_0805ba40[] = "You deflected two rocks in one go! Brilliant!";
+const char D_0805ba40[] = "Vocˆ® desviou duas pedras de uma s‡i vez! Brilhante!";
 
-const char D_0805ba68[] = "My Girlfriend Calmly Comments...";
+const char D_0805ba68[] = "Minha Namorada Comenta Calmamente...";
