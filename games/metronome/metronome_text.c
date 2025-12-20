feed241680a0807b88e34444085599c4593c48a9
Nyoka <davijoaquimsilvacosta2@gmail.com>
2025-12-20 10:59:39 -0300

    Add files via upload

--- games/metronome/metronome_text.c
+++ games/metronome/metronome_text.c
@@ -5,12 +5,12 @@
 /* Game Text - Metronome */
 
 
-const char D_0805ccc8[] = "You'll have to put in a lot of effort...";
+const char D_0805ccc8[] = "Vocˆ® ter‡c que se esforˆ¹ar bastante...";
 
-const char D_0805cce4[] = "You'll need to try a little harder.";
+const char D_0805cce4[] = "Vocˆ® precisar‡c se esforˆ¹ar um pouco mais.";
 
-const char D_0805cd04[] = "You're as accurate as any person could be.";
+const char D_0805cd04[] = "Vocˆ® ‡Q tˆºo preciso quanto qualquer pessoa poderia ser.";
 
-const char D_0805cd24[] = "You have an accurate biological clock!";
+const char D_0805cd24[] = "Vocˆ® tem um rel‡igio biol‡igico preciso!";
 
-const char D_0805cd44[] = "Incredible! You're as precise as an atomic clock!";
+const char D_0805cd44[] = "Incr‡evel! Vocˆ® ‡Q tˆºo preciso quanto um rel‡igio atˆ¶mico!";
